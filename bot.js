4ddaa9d3601b710e6f8c4b16930c516a96f28a28
C0DE8REAKER <32065190+MitchellArtz@users.noreply.github.com>
2017-09-29 18:55:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ let apitoken = ("process.env.BOT_TOKEN")
 
 bot.on('ready', () => {
     console.log('Logged in as ' + bot.user.tag + ' and I am on ' + bot.guilds.size + ' guilds!')
-    bot.user.setGame("${config.prefix}help | ${bot.guilds.size} servers!", "https://www.twitch.tv/Blank");
+    bot.user.setGame("+{config.prefix}help | +{bot.guilds.size} servers!", "https://www.twitch.tv/Blank");
 });
 
 bot.on("guildCreate", guild => {

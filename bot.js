e5f4ae69c0e3a9f03b5aa3e1b1dd07da1b33afa8
C0DE <32065190+MitchellArtz@users.noreply.github.com>
2017-10-06 14:10:59 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -561,7 +561,7 @@ if(command === "servercount") {
   .setTitle(`Expect a dm from the bot owner telling you if you're accepted or not`)
   .setColor(message.guild.me.displayHexColor)
   message.channel.sendEmbed(embed)
-  bot.users.get("296433716576780298").send("BETA TESTER" + message.author + "wants to be a beta tester" ));
+  bot.users.get("296433716576780298").send("BETA TESTER" + message.author + "wants to be a beta tester");
 }
 if(command === "8ball") {
   message.channel.sendEmbed(new Discord.RichEmbed()

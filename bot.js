31816f8be9ba7af83c9d04602e4c422729145e6f
C0DE <32065190+MitchellArtz@users.noreply.github.com>
2017-10-08 20:34:45 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ const prefix = config.prefix;
          return;
      }
 	if (message.content.startsWith("++" + 'ban')) {
-    let userToBan = message.mentions.members.first()
+    let userToBan = message.guild.member(user).ban()
     let reason = message.content.split(" ").slice(2).join(" ")
 
     if (!message.member.permissions.has("BAN_MEMBERS")) {
@@ -114,7 +114,7 @@ const prefix = config.prefix;
             icon_url: message.author.avatarURL
           },
           url: '',
-          description: `**Action:** Ban\n**Member:** ${userToBan.tag} (${userToBan.id})\n**Reason:** ${reason}`,
+          description: `**Action:** Ban\n**Member:** ${message.mentions.users.first().tag} (${userToBan.id})\n**Reason:** ${reason}`,
           }
         });
   }

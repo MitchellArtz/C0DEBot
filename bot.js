df69fbb322e1c0c3f1cd9fb158cc9a984169939a
C0DE <32065190+MitchellArtz@users.noreply.github.com>
2017-10-08 19:46:30 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,6 +86,26 @@ const prefix = config.prefix;
          message.reply('I cannot respond with DMs!')
          return;
      }
+	 module.exports.run = async (bot, msg, args) => {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!msg.member.permissions.has("BAN_MEMBERS"))
+      return msg.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = msg.mentions.members.first();
+    if(!member)
+      return msg.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return msg.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1);
+    if(!reason)
+      return msg.reply("Please indicate a reason for the ban!");
+    
+    await member.ban(reason)
+      .catch(error => msg.reply(`Sorry ${msg.author} I couldn't ban because of : ${error}`));
+    msg.reply(`${member.user.tag} has been banned by ${msg.author.tag} because: ${reason}`);
+}
 
   if (command === "ping") {
     if(isBlacklisted(message.author.id)) return message.channel.sendMessage("Sorry, but you are Blacklisted from this bot!");

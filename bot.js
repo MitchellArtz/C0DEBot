21df763604a960b87f727c3ced672ff762dd68f9
Mitchell Artz <32065190+MitchellArtz@users.noreply.github.com>
2018-10-27 19:53:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -730,5 +730,22 @@ function doRandomSize(){
 
 }
 
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
 
+  if (message.content.startsWith(config.prefix + "eval2")) {
+    if(message.author.id !== config.ownerid2) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
 bot.login(process.env.BOT_TOKEN)

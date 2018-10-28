72523d9465d61329bdf77601e9cfd440de928a7e
Mitchell Artz <32065190+MitchellArtz@users.noreply.github.com>
2018-10-27 20:05:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -755,35 +755,5 @@ bot.on("message", message => {
 
 
 
-if (command === "eval3"){
-    if (message.author.id !== config.ownerid2) return message.reply("You do not have permission to use this.")
-var str = args.join(" ");
-var patt = new RegExp("token");
-var res = patt.test(str);
-
-if(res === true) return message.reply(config.token)
-
-//if(!isAdmin(message.author.id)) return message.reply("You do not have permission to use this command.")
-
-try {
-
-  var code = args.join(" ");
-  var evaled3 = eval(code);
-
-  if (typeof evaled3 !== "string")
-    evaled3 = require("util").inspect(evaled3);
-  //message.channel.sendMessage(":inbox_tray: **INPUT**\n")
-  message.channel.sendEmbed(new Discord.RichEmbed().addField("Javascript Eval:", "Success!").addField(":inbox_tray: **INPUT**", "```" + args.join(" ") + "```").addField(":outbox_tray: **OUTPUT**", "```" + clean(evaled3) + "```").setColor(message.guild.me.displayHexColor))
-  //message.channel.sendCode("xl", args.join(" "));
-  //message.channel.sendMessage(":outbox_tray: **OUTPUT**\n")
-
-  //message.channel.sendCode("xl", clean(evaled));
-
-} catch (err){
-
-  message.channel.sendEmbed(new Discord.RichEmbed().addField("Javascript Eval ERROR:", "There was a problem with the code your trying to run!").addField("Error", "```" + clean(err) + "```").setColor(message.guild.me.displayHexColor))
-  //message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-}
-}
 
 bot.login(process.env.BOT_TOKEN)
